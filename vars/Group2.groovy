c9d6280ea31ba33fb937769bd1ff97fdf0c52363
Etechteam7 <abiola.abimbola@yahoo.co.uk>
2023-10-14 23:16:35 -0400

    group2 shared library

---
+++ vars/Group2.groovy
@@ -0,0 +1,60 @@
+
+def call(String repoUrl){
+    pipeline{
+	agent any
+	stages{
+		stage('Build'){
+			steps{
+				sh 'systemctl status jenkins'
+			}
+		}
+		stage('Test'){
+			parallel{
+				stage('unitest'){
+					steps{
+						sh 'df -h'
+						sh 'cat /etc/passwd'
+					}
+
+				}
+				
+			}
+		}
+		stage('Deploy'){
+			steps{
+				sh'ls -l'
+			}
+		}
+		stage('Release'){
+			parallel{
+				stage('Release to staging'){
+					steps{
+						sh 'lscpu'
+					}
+
+				}
+					
+				stage('Release to production'){
+					steps{
+						sh 'free -m'
+				}
+				
+			}
+			}
+			stage('Notify'){
+				steps{
+					sh 'ls -lh'
+				}
+			}
+			stage('Securitytest'){
+				steps{
+					sh 'pwd'
+				}
+			}
+		}
+}
+}
+
+	
+
+}
